4a8fd4ae990105fa0e72b7d4869d9940a8469962
BKFighter <bkfergen@gmail.com>
2016-08-28 19:30:19 -0500

    Update search-bar-position.js

--- search-bar-position.js
+++ search-bar-position.js
@@ -5,5 +5,10 @@ searchClass[0].addEventListener("resize", repositionSearch);
 function repositionSearch(){
 
 
+var wWidth = window.innerWidth;
+var wHeight = window.innerHeight;
+var sWidth = searchClass[0].offsetWidth;
+var sHeight = searchClass[0].offsetHeight;
+console.log("window width"+wWidth+"window height"+wHeight+"search width"+sWidth+"search height"+sHeight);
 
 }
