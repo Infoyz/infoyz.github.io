3dda7b18a93c82098ee45323ca21981df1765dbb
BKFighter <bkfergen@gmail.com>
2016-08-28 19:22:45 -0500

    Create search-bar-position.js

---
+++ search-bar-position.js
@@ -0,0 +1,9 @@
+window.addEventListener("resize", repositionSearch);
+var searchClass = document.getElementsById("middle-page-search-bar");
+searchClass[0].addEventListener("resize", repositionSearch);
+
+function repositionSearch(){
+
+
+
+}
