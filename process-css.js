d0aa075c029c8873b9f8ba2f8cd225576ea3e1c9
BKFighter <bkfergen@gmail.com>
2016-09-03 14:40:34 -0500

    Update process-css.js

--- process-css.js
+++ process-css.js
@@ -1,3 +1,4 @@
 var colors = ['#93FF19','#93FF19','#93FF19','#196DFF','#FFb619'];
 var main = colors[Math.floor((Math.random()*5)+1)];
-process('https://rawgit.com/Infoyz/infoyz.github.io/master/desk-lap.ppcss',main,null);
+console.log(main);
+window.onload=function(){ process('https://rawgit.com/Infoyz/infoyz.github.io/master/desk-lap.ppcss',main,null); };
