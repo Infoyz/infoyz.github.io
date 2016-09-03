6af7d28a2aca81c124d780c2b86a44a5b6a5f263
BKFighter <bkfergen@gmail.com>
2016-09-03 12:16:50 -0500

    Update query-search.js

--- query-search.js
+++ query-search.js
@@ -1,17 +1,27 @@
-function query(){
-  
-    var xmlHttp = new XMLHttpRequest();
-    
-    
-    
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            console.log(xmlHttp.responseText);
+var xmlHttp = null;
+
+function GetCustomerInfo()
+{
+    //var query = document.getElementById( "middle-page-search-bar" ).value;
+    //var url = "infoyz-bkfighter.rhcloud.com" + query;
+    var url = infoyz-bkfighter.rhcloud.com;
+
+    xmlHttp = new XMLHttpRequest(); 
+    xmlHttp.onreadystatechange = ProcessRequest;
+    xmlHttp.open( "GET", url, true );
+    xmlHttp.send( null );
+}
+
+function ProcessRequest() 
+{
+    if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ) 
+    {
+        if ( xmlHttp.responseText == "Not found" ) 
+        {
+            var info = eval ( "(" + xmlHttp.responseText + ")" );
+
+            // No parsing necessary with JSON!        
+            document.write(info.jsonData[ 0 ]);
+        }                    
     }
-    
-    
-    
-    xmlHttp.open("GET", infoyz-bkfighter.rhcloud.com, true); // true for asynchronous 
-    xmlHttp.send(null);
-    
 }
