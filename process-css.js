4ca46b158f7bc7b53a1dd79718e92e84779fcaf2
BKFighter <bkfergen@gmail.com>
2016-09-03 14:29:07 -0500

    Create process-css.js

---
+++ process-css.js
@@ -0,0 +1,3 @@
+var colors = ['#93FF19','#93FF19','#93FF19','#196DFF','#FFb619'];
+var main = colors[Math.floor((Math.random()*5)+1)];
+process('https://rawgit.com/Infoyz/infoyz.github.io/master/desk-lap.ppcss',main,null);
