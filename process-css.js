a380d130ae0d8768e3d7590ea11cf83fa6c1979d
BKFighter <bkfergen@gmail.com>
2016-09-04 14:52:22 -0500

    Update process-css.js

--- process-css.js
+++ process-css.js
@@ -1,4 +1,4 @@
-var colors = ['#93FF19','#93FF19','#93FF19','#196DFF','#FFb619'];
-var main = colors[Math.floor(Math.random()*5)];
+var colors = ['#93FF19','#93FF19','#93FF19','#196DFF','#196DFF','#FFb619','#FFb619'];
+var main = colors[Math.floor(Math.random()*7)];
 console.log(main);
 window.onload=function(){ process('https://rawgit.com/Infoyz/infoyz.github.io/master/desk-lap.ppcss',main,null); };
