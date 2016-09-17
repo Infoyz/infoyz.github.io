25756fc827874b277c0ec9f71e51ca76c7388164
BKFighter <bkfergen@gmail.com>
2016-09-17 16:14:01 -0500

    Create searchStart.js

---
+++ searchStart.js
@@ -0,0 +1,25 @@
+document.getElementById('middle-page-search-bar').onkeypress = function(e){
+    if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+      
+      searchStart(document.getElementById('middle-page-search-bar').value);
+      
+      return false;
+    }
+  }
+  
+  function searchStart(search){
+  
+  var isMath = /^[0-9.()*/+-]*$/.test(search);
+  
+  if(isMath){
+  
+  var mathResult = eval(search);
+  console.log(mathResult);
+  
+  }
+  
+  
+  
+  }
