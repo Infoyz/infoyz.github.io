556a0a9e7631136694c14c8ea59acce71fb95161
BKFighter <bkfergen@gmail.com>
2016-09-23 17:16:40 -0500

    Update display-boxes.js

--- display-boxes.js
+++ display-boxes.js
@@ -15,7 +15,41 @@ if(type == 'math'){
     
 }
 
+
+updatePositions();
+
 }
+
+
+
+
+function updatePositions(){
+    
+    var dispboxes = document.getElementsByClassName('display-boxes');
+    var amount = dispboxes.length;
+    
+    var wWidth = window.innerWidth;
+    var wHeight = window.innerHeight;
+    var sWidth = searchId.offsetWidth;
+    var sHeight = searchId.offsetHeight;
+    var hHeight = document.getElementById("page-header").offsetHeight;
+    
+    var verticalSpacing = (wHeight-hHeight-sHeight)/2;
+    var horizontalSpacing = (wWidth-sWidth)/2;
+    var middleX = (wWidth/2)-(sWidth/2);
+    var middleY = ((((wHeight-hHeight)/2)-(sHeight/2))+hHeight);
+
+    
+    for var(i=0;i<amount;i++){
+        
+        currentBox = dispboxes[i];
+        
+        
+    }
+    
+}
+
+
 
 
 
