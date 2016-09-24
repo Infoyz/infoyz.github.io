81df774b3791007d252b85b7c6074b0d9717439c
BKFighter <bkfergen@gmail.com>
2016-09-24 09:03:15 -0500

    Update display-boxes.js

--- display-boxes.js
+++ display-boxes.js
@@ -49,8 +49,8 @@ function updatePositions(){
         var secondX = Math.cos(angle)*wWidth;
         var secondY = Math.sin(angle)*(wHeight-hHeight)+hHeight;
         
-        currentBox.style.top = (firstY+secondY)/2;
-        currentBox.style.left = (firstX+secondX)/2;
+        currentBox.style.top = (firstY+secondY)/2 + 'px';
+        currentBox.style.left = (firstX+secondX)/2 + 'px';
         
     }
     
